ca3dd477e4d8b91ce9de24be82fc91221a0b1d0e
filipeam <afilipem@gmail.com>
2019-11-30 20:15:44 -0600

    added: code that shows the dialog with the oneselectionoption

--- app/src/main/java/com/example/clientserverassignment/MainActivity.java
+++ app/src/main/java/com/example/clientserverassignment/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.clientserverassignment;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final CharSequence[] fieldOptions = new CharSequence[] {"All", "Title", "Author", "Publisher", "Year"};
+        //loading the images
+        ImageView viewDatabaseImageView = this.findViewById(R.id.viewDatabaseImageView);
+        viewDatabaseImageView.setImageResource(R.drawable.database_view);
+        ImageView editDatabaseImageView = this.findViewById(R.id.editDatabaseImageView);
+        editDatabaseImageView.setImageResource(R.drawable.database_edit);
+        ImageView deleteDatabaseImageView = this.findViewById(R.id.deleteDatabaseImageView);
+        deleteDatabaseImageView.setImageResource(R.drawable.database_delete);
+        //creating the dialogs with the options
+        viewDatabaseImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertdialogbuilder =
+                        new AlertDialog.Builder(MainActivity.this);
+                alertdialogbuilder.setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int id) {
+                                //code for the database operation
+                            }
+                        });
+                alertdialogbuilder.setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int id) {
+                                // User cancelled the dialog
+                            }
+                        });
+
+                alertdialogbuilder.setSingleChoiceItems(fieldOptions, 0,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                                int which) {
+                                Toast.makeText(getBaseContext(), fieldOptions[which]+ " selected", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                AlertDialog dialog = alertdialogbuilder.create();
+                dialog.show();
+            }
+
+        });
     }
 }
